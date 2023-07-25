6078e3d5edbe632198055928027f6bcf50d42079
Breno Raeder <brenoraeder97@gmail.com>
2023-07-25 18:49:00 -0300

    primeiro commit

---
+++ arquivo/src/test/java/com/api/arquivo/ArquivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.arquivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
